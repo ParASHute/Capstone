52b88ec4dcce40f3a8096254563381eed2a7f922
ParASHute <ktwykhs@naver.com>
2023-05-19 16:36:31 +0900

    UI추가

---
+++ MyApplication2/app/src/main/java/com/example/myapplication/list 2.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+public class list {
+    private String bilding;
+    private String info;
+    private String floor;
+    public list(){}
+    public String getBilding(){
+        return  bilding;
+    }
+    public void setBilding(String bilding) {
+        this.bilding = bilding;
+    }
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getFloor() {
+        return floor;
+    }
+
+    public void setFloor(String floor) {
+        this.floor = floor;
+    }
+}
