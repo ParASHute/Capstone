d144b7e4b8a38231d0ff9c255cde2ce495627938
youngrockchoi <cho990205@naver.com>
2023-05-22 23:09:59 +0900

    ui수정

--- app/src/main/java/com/example/capston/List.java
+++ app/src/main/java/com/example/capston/List.java
@@ -1,36 +1,32 @@
 package com.example.capston;
 
 public class List {
+    private String Profile;
     private String Build;
     private String Office;
     private String Floor;
 
-   // private double Lat;
-   // private double Lon;
-
     public List(){}
-
-    public void setFloor(String floor) {
-        this.Floor = floor;
-    }
-
-    public void setInfo(String Office) {
-        this.Office = Office;
-    }
-
-    public void setBuild(String Build) {
-        this.Build = Build;
-    }
-
+    // private double Lat;
+    // private double Lon;
+    public String getProfile(){return Profile;}
+    public  void setProfile(String profile) {this.Profile = profile;}
     public String getFloor() {
         return Floor;
     }
-
+    public void setFloor(String floor) {
+        this.Floor = floor;
+    }
     public String getOffice() {
         return Office;
     }
-
+    public void setInfo(String Office) {
+        this.Office = Office;
+    }
     public String getBuild() {
         return Build;
+    }
+    public void setBuild(String Build) {
+        this.Build = Build;
     }
 }
